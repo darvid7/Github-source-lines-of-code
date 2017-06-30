dc429245a9dbe78fb3e61165bdd9d4d050d5c0ae
darvid7 <bvdarvid@gmail.com>
2017-06-30 14:17:00 +1000

    Remove gitwarnings in stdout

--- scripts/cloc-git-test.sh
+++ scripts/cloc-git-test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-git clone --depth 1 "$1" temp-linecount-repo -q &&
-  cloc temp-linecount-repo --json --report-file="./data/data.json" --quiet &&
-  rm -rf temp-linecount-repo
+git clone --depth 1 "$1" temp-sloc-count-repo --quiet 2> gitwarnings.txt &&
+  cloc temp-sloc-count-repo --json --report-file="./data/data.json" --quiet  &&
+  rm -rf temp-sloc-count-repo
